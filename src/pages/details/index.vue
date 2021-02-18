dc8c4f5ef5c4f656efb308689d725b66898436d7
moshubai <939761443qq.com>
2021-02-18 23:00:41 +0800

    1

---
+++ src/pages/details/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="details">
+    详情页面
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'leaderboard-page',
+    components: {},
+    data () {
+      return {
+
+      }
+    },
+    methods: {},
+    mounted () {}
+  }
+</script>
+
+<style lang="less" scoped>
+.details {
+  width: 100%;
+//   display: flex;
+//   display: -webkit-flex;
+
+}
+
+</style>
